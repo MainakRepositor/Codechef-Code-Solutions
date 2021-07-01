3e4341f1558c16451ee4119a054171b9a6ad5f38
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-07-01 10:50:15 +0530

    Create FLOW002.c

---
+++ FLOW002.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main() {
+	// Read the number of test cases.
+	int T;
+	scanf("%d", &T);
+	while (T--) {
+		// Read the input a, b
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		// Compute the ans.
+		// Complete this line.
+		int ans = a%b;
+		printf("%d\n", ans);
+	}
+
+	return 0;
+}
