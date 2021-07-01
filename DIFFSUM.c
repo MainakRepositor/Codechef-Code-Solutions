7c21c6e818260caa50cf929dbfa1916f18ccef36
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-07-01 11:05:07 +0530

    Create DIFFSUM.c

---
+++ DIFFSUM.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if(a>b)
+	{
+	    printf("%d\n",a-b);
+	}
+	else
+	{
+	    printf("%d\n",a+b);
+	}
+	
+	return 0;
+}
+
