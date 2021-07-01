7d88165a1d8ffb4d853d079e675fd9e65a4e0680
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-07-01 10:53:30 +0530

    Create FSQRT.c

---
+++ FSQRT.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	while(n--)
+	{
+	    int a=0, s=0;
+	    scanf("%d",&a);
+	    printf("%d\n",(int)(sqrt(a)));
+	}
+	
+	return 0;
+}
+
